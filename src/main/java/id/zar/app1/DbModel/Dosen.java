e09770cbbe2deaa13de32744eb0d195881f7fcb1
mohammad.zarkasi <mohammad.zarkasi@gmail.com>
2023-09-21 22:20:47 +0700

    gunakan mysql

--- src/main/java/id/zar/app1/DbModel/Dosen.java
+++ src/main/java/id/zar/app1/DbModel/Dosen.java
@@ -1,17 +1,31 @@
 package id.zar.app1.DbModel;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
+@Table(name = "dosen")
 public class Dosen {
+    @Id
     private String id;
     private String nama;
     private String prodi;
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+    @UpdateTimestamp
+    private LocalDateTime updatedOn;
 }
